8ab9aa27bda60dd8d41cceb097d3a725ba3ff033
TomKim <tosh0948@gmail.com>
2015-07-13 16:29:09 +0300

    Resolved serialization problem

--- src/main/java/com/icpak/rest/providers/ICPAKSerializationModule.java
+++ src/main/java/com/icpak/rest/providers/ICPAKSerializationModule.java
@@ -13,35 +13,36 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 
 public class ICPAKSerializationModule extends SimpleModule {
 
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 	public ICPAKSerializationModule() {
-        super("ObjectSerializerModule", new Version(0, 1, 0, "alpha", "Groupid","ArtifactId"));
-        this.addSerializer(Date.class, new DateSerializer());
-    }
+		super("ObjectSerializerModule", new Version(0, 1, 0, "alpha",
+				"Groupid", "ArtifactId"));
+		this.addSerializer(Date.class, new DateSerializer());
+	}
 
-    /**
-     * 
-     * Date Serializer
-     * 
-     * @author duggan
-     *
-     */
-    public class DateSerializer extends JsonSerializer<Date> {
+	/**
+	 * 
+	 * Date Serializer
+	 * 
+	 * @author duggan
+	 *
+	 */
+	public class DateSerializer extends JsonSerializer<Date> {
 
-    	private final SimpleDateFormat dateFormat = new SimpleDateFormat(
-    			"yyyy-MM-dd");
+		private final SimpleDateFormat dateFormat = new SimpleDateFormat(
+				"yyyy-MM-dd");
 
-    	@Override
-    	public void serialize(Date date, JsonGenerator gen,
-    			SerializerProvider provider) throws IOException,
-    			JsonProcessingException {
-    		String formattedDate = dateFormat.format(date);
-    		gen.writeString(formattedDate);
-    	}
-    }
+		@Override
+		public void serialize(Date date, JsonGenerator gen,
+				SerializerProvider provider) throws IOException,
+				JsonProcessingException {
+			// String formattedDate = dateFormat.format(date);
+			gen.writeNumber(date.getTime());
+		}
+	}
 
 }
