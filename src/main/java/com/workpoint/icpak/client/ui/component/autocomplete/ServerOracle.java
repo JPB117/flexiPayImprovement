bd4fa8ea691887bb746a307e1cd1bbef8772182b
duggankimani <mdkimani@gmail.com>
2015-10-27 15:48:54 +0300

    Server Oracle, Added search delay

--- src/main/java/com/workpoint/icpak/client/ui/component/autocomplete/ServerOracle.java
+++ src/main/java/com/workpoint/icpak/client/ui/component/autocomplete/ServerOracle.java
@@ -3,27 +3,38 @@ package com.workpoint.icpak.client.ui.component.autocomplete;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gwt.user.client.Timer;
 import com.workpoint.icpak.client.ui.component.AutoCompleteField;
 import com.workpoint.icpak.shared.model.Listable;
 
 public class ServerOracle<T extends Listable> extends SimpleOracle<T> {
 
+	String searchTerm = "";
 	private Request request;
 	private Callback callback;
 	private List<StoreChangedHandler<T>> handlers = new ArrayList<StoreChangedHandler<T>>();
 	private AutoCompleteField.Loader<T> loader;
-	
+
+	Timer timer = new Timer() {
+
+		@Override
+		public void run() {
+			loader.onLoad(ServerOracle.this, searchTerm);
+		}
+		
+	};
+
 	public ServerOracle(AutoCompleteField.Loader<T> loader) {
 		this.loader = loader;
 	}
-	
+
 	@Override
 	public void requestSuggestions(Request request, Callback callback) {
 		this.request = request;
 		this.callback = callback;
-		String query = request.getQuery();
-		loader.onLoad(this,query);
-		
+		searchTerm = request.getQuery();
+		timer.cancel();
+		timer.schedule(400);
 	}
 
 	public void setValues(List<T> values) {
@@ -41,21 +52,21 @@ public class ServerOracle<T extends Listable> extends SimpleOracle<T> {
 			resp.setSuggestions(suggestions); // every server result fits the
 												// query
 			callback.onSuggestionsReady(request, resp);
-			
-			//Reset
-			request=null;
-			resp=null;
-			for(StoreChangedHandler<T> handler:handlers){
+
+			// Reset
+			request = null;
+			resp = null;
+			for (StoreChangedHandler<T> handler : handlers) {
 				handler.onStoreChanged(values);
 			}
 		}
 	}
-	
-	public void addStoreChangedHandler(StoreChangedHandler<T> handler){
+
+	public void addStoreChangedHandler(StoreChangedHandler<T> handler) {
 		handlers.add(handler);
 	}
-	
-	public static interface StoreChangedHandler<T>{
+
+	public static interface StoreChangedHandler<T> {
 		public void onStoreChanged(List<T> values);
 	}
 }
