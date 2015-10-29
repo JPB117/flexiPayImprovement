a6dcb0a2e21b4e6c1e8f3748e9e0b1a2bffd0f2d
Tom Kimani <tosh0948@gmail.com>
2015-10-29 09:54:32 +0300

    Some changes in delegates not tracked

---
+++ src/main/java/com/workpoint/icpak/shared/api/DelegatesResource.java
@@ -0,0 +1,24 @@
+package com.workpoint.icpak.shared.api;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.workpoint.icpak.shared.model.events.DelegateDto;
+
+public interface DelegatesResource extends BaseResource {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<DelegateDto> getAll(@QueryParam("offset") Integer offset, @QueryParam("limit") Integer limit,
+			@QueryParam("searchTerm") String searchTerm);
+
+	@GET
+	@Path("/searchCount")
+	public Integer getSearchCount(@QueryParam("searchTerm") String searchTerm);
+
+}
