0dd403626d69b75e233e53c5b4fbd9b1b4f08151
Tom Kimani <tosh0948@gmail.com>
2015-11-20 09:45:06 +0300

    Executive Seminar Importation

--- src/main/java/com/icpak/rest/dao/BookingsDao.java
+++ src/main/java/com/icpak/rest/dao/BookingsDao.java
@@ -69,7 +69,7 @@ public class BookingsDao extends BaseDao {
 	}
 
 	public void importAllEvents() {
-		String sqlQuery = "select oldSystemId,refId from icpakdb.event where oldSystemId=477";
+		String sqlQuery = "select oldSystemId,refId from icpakdb.event where oldSystemId=424";
 		// String sqlQuery =
 		// "select oldSystemId,refId from icpakdb.event where oldSystemId=424";
 		List<Object[]> rows = getResultList(getEntityManager()
