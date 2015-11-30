51febb8d0b5c104d3c3aac8dc2b06cd6fb0b81d0
wladek.airo@gmail.com <wladek.airo@gmail.com>
2015-11-30 20:03:16 +0300

    overseas memberUi

--- src/main/java/com/icpak/rest/dao/BaseDao.java
+++ src/main/java/com/icpak/rest/dao/BaseDao.java
@@ -13,116 +13,139 @@ import com.icpak.rest.exceptions.ServiceException;
 import com.icpak.rest.models.ErrorCodes;
 import com.icpak.rest.models.base.PO;
 
-
 public class BaseDao {
-	
-	@Inject Provider<EntityManager> provider;
-	public EntityManager getEntityManager(){
+
+	@Inject
+	Provider<EntityManager> provider;
+
+	public EntityManager getEntityManager() throws RuntimeException {
 		EntityManager em = provider.get();
 		return em;
 	}
-	
-	public void save(PO po){
-		getEntityManager().persist(po);
+
+	public void save(PO po) throws RuntimeException {
+		try {
+			getEntityManager().persist(po);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
-	
-	public void flush(){
-		getEntityManager().flush();
+
+	public void flush() throws RuntimeException {
+		try {
+			getEntityManager().flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public void merge(PO po) {
-		getEntityManager().merge(po);
+		try {
+			getEntityManager().merge(po);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
-	
-	public void delete(PO po){
-		getEntityManager().remove(po);
+
+	public void delete(PO po) throws RuntimeException {
+		try {
+			getEntityManager().remove(po);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public <T> T getSingleResultOrNull(Query query){
+	public <T> T getSingleResultOrNull(Query query) throws RuntimeException {
 		T value = null;
-		try{
-			value = (T)query.getSingleResult();
-		}catch(Exception e){
-			if(!(e instanceof NoResultException)){
+		try {
+			value = (T) query.getSingleResult();
+		} catch (Exception e) {
+			if (!(e instanceof NoResultException)) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return value;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public <T> List<T> getResultList(Query query,Integer offSet, Integer limit){
+	public <T> List<T> getResultList(Query query, Integer offSet, Integer limit) throws RuntimeException {
 		List<T> values = null;
-		
-		if(limit==null || offSet==null){
-			values = query.getResultList();
-		}else{
-			values = query.setFirstResult(offSet).setMaxResults(limit).getResultList();
+
+		try {
+			if (limit == null || offSet == null) {
+				values = query.getResultList();
+			} else {
+				values = query.setFirstResult(offSet).setMaxResults(limit).getResultList();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
+
 		return values;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public <T> List<T> getResultList(Query query){
+	public <T> List<T> getResultList(Query query) throws RuntimeException {
 		List<T> values = null;
-		values = query.getResultList();
+		try{
+			values = query.getResultList();
+		}catch (Exception e){
+			e.printStackTrace();
+		}
 		return values;
 	}
-	
-	public <T> T getById(Class<T> clazz, long id){
-		
+
+	public <T> T getById(Class<T> clazz, long id) {
 		return getEntityManager().find(clazz, id);
 	}
-	
-	public <T> T findByRef(Class<?> clazz, String refId, boolean throwExceptionIfNull) {
-		T po = getSingleResultOrNull(
-				getEntityManager().createQuery("from "+clazz.getName()+" u where u.refId=:refId")
-				.setParameter("refId", refId));
-		
-		if(po==null && throwExceptionIfNull){
-			throw new ServiceException(ErrorCodes.NOTFOUND,clazz.getName(), "'"+refId+"'");
+
+	public <T> T findByRef(Class<?> clazz, String refId, boolean throwExceptionIfNull) throws RuntimeException {
+		T po = getSingleResultOrNull(getEntityManager()
+				.createQuery("from " + clazz.getName() + " u where u.refId=:refId").setParameter("refId", refId));
+
+		if (po == null && throwExceptionIfNull) {
+			throw new ServiceException(ErrorCodes.NOTFOUND, clazz.getName(), "'" + refId + "'");
 		}
-		
+
 		return po;
 	}
 
-	public <T> T findByRefId(String refId, Class<?> clazz){
+	public <T> T findByRefId(String refId, Class<?> clazz) throws RuntimeException {
 		return findByRefId(refId, clazz, true);
 	}
-	
-	public <T> T findByRefId(String refId, Class<?> clazz, boolean throwExceptionIfNull) {
+
+	public <T> T findByRefId(String refId, Class<?> clazz, boolean throwExceptionIfNull) throws RuntimeException {
 		return findByRefId(refId, clazz, new HashMap<String, Object>(), throwExceptionIfNull);
 	}
-	
+
 	public <T> T findByRefId(String refId, Class<?> clazz, Map<String, Object> params, boolean throwExceptionIfNull) {
-		
-		StringBuffer buff = new StringBuffer("from "+clazz.getName()+" c where c.refId=:refId");
-		
-		//Variables
-		if(params!=null){
-			for(String key: params.keySet()){
-				buff.append(" and "+key+"=:"+key);
+
+		StringBuffer buff = new StringBuffer("from " + clazz.getName() + " c where c.refId=:refId");
+
+		// Variables
+		if (params != null) {
+			for (String key : params.keySet()) {
+				buff.append(" and " + key + "=:" + key);
 			}
 		}
-		Query query = getEntityManager().createQuery(buff.toString())
-				.setParameter("refId", refId);
-		//Params
-		if(params!=null){
-			for(String key: params.keySet()){
+		Query query = getEntityManager().createQuery(buff.toString()).setParameter("refId", refId);
+		// Params
+		if (params != null) {
+			for (String key : params.keySet()) {
 				query.setParameter(key, params.get(key));
 			}
 		}
-		
+
 		T rtn = getSingleResultOrNull(query);
-		if(rtn==null && throwExceptionIfNull){
-			throw new ServiceException(ErrorCodes.NOTFOUND,clazz.getName(), "'"+refId+"'");
+		if (rtn == null && throwExceptionIfNull) {
+			throw new ServiceException(ErrorCodes.NOTFOUND, clazz.getName(), "'" + refId + "'");
 		}
-		
+
 		return rtn;
 	}
-
 
 }
