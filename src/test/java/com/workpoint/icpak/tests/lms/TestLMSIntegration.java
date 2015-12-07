48528512dfdb6a59c55c3bb0db1bcff4ef47bdc7
wladek.airo@gmail.com <wladek.airo@gmail.com>
2015-12-07 18:00:33 +0300

    add lms test

--- src/test/java/com/workpoint/icpak/tests/lms/TestLMSIntegration.java
+++ src/test/java/com/workpoint/icpak/tests/lms/TestLMSIntegration.java
@@ -3,22 +3,36 @@ package com.workpoint.icpak.tests.lms;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.ws.rs.core.MediaType;
 
 import org.junit.Ignore;
+import org.junit.Test;
 
+import com.amazonaws.util.json.JSONException;
+import com.google.inject.Inject;
+import com.icpak.rest.dao.BookingsDao;
+import com.icpak.rest.dao.helper.BookingsDaoHelper;
+import com.icpak.rest.models.event.Delegate;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+import com.workpoint.icpak.server.integration.lms.LMSIntegrationUtil;
 import com.workpoint.icpak.shared.lms.LMSMemberDto;
 import com.workpoint.icpak.shared.model.Gender;
 import com.workpoint.icpak.shared.model.Title;
+import com.workpoint.icpak.shared.model.events.DelegateDto;
+import com.workpoint.icpak.tests.base.AbstractDaoTest;
 
-public class TestLMSIntegration {
+public class TestLMSIntegration extends AbstractDaoTest {
+	@Inject
+	BookingsDao dao;
+	@Inject
+	BookingsDaoHelper helper;
 
-	
 	@Ignore
 	public void sendMemberData() throws IOException {
 		// System.out.println("SysResponse = " + response);
@@ -50,11 +64,9 @@ public class TestLMSIntegration {
 		dto.setMembershipID("10000");
 		dto.setRefID("48fd4a84f8da");
 
-		WebResource resource = client
-				.resource("http://localhost:8080/messenger/webapi/messages");
+		WebResource resource = client.resource("http://localhost:8080/messenger/webapi/messages");
 
-		ClientResponse resp = resource.type(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON)
+		ClientResponse resp = resource.type(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
 				.post(ClientResponse.class, dto);
 
 		String out = resp.getEntity(String.class);
@@ -66,7 +78,23 @@ public class TestLMSIntegration {
 		}
 
 	}
-	
-//	@Test
-//	public void testIn
+
+	@Test
+	public void testEnrolDelegateToLms() {
+		List<DelegateDto> delegates = new ArrayList<>();
+		Delegate delegateInDb = dao.findByRefId("h8IeWNUed7DckOgY", Delegate.class);
+		DelegateDto dto = delegateInDb.toDto();
+		dto.setCourseId("22");
+
+		delegates.add(dto);
+
+		try {
+			helper.enrolDelegateToLMS(delegates, "YeVcDDmtM7th2SgR");
+		} catch (JSONException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// @Test
+	// public void testIn
 }
