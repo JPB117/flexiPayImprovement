7f09cc469e038087df2c428c55a1e2b01638f400
Tom Kimani <tosh0948@gmail.com>
2016-02-22 01:16:02 +0300

    Roles changes in the dto

--- src/main/java/com/workpoint/icpak/shared/model/RoleDto.java
+++ src/main/java/com/workpoint/icpak/shared/model/RoleDto.java
@@ -2,27 +2,26 @@ package com.workpoint.icpak.shared.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-
-public class RoleDto extends SerializableObj implements Listable{
+public class RoleDto extends SerializableObj implements Listable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private Long id;
 	private String name;
 	private String fullName;
-	private Long UserTypeID;
-	private Long RoleId;
-	
-	public RoleDto(){
+	private Long userTypeID;
+	private Long roleId;
+
+	public RoleDto() {
 	}
 
-	public RoleDto(String name){
-		this.name=name;
+	public RoleDto(String name) {
+		this.name = name;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
@@ -46,49 +45,48 @@ public class RoleDto extends SerializableObj implements Listable{
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
-		if(!(obj instanceof RoleDto)){
+		if (!(obj instanceof RoleDto)) {
 			return false;
 		}
-		
-		RoleDto other = (RoleDto)obj;
-		
+
+		RoleDto other = (RoleDto) obj;
+
 		return other.name.equals(name);
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return name.hashCode();
 	}
-	
+
 	@Override
 	public String toString() {
-		
+
 		return name;
 	}
 
 	@JsonIgnore
 	public String getDisplayName() {
-		
 		return fullName;
 	}
 
 	public Long getUserTypeID() {
-		return UserTypeID;
+		return userTypeID;
 	}
 
 	public void setUserTypeID(Long userTypeID) {
-		UserTypeID = userTypeID;
+		userTypeID = userTypeID;
 	}
 
 	public Long getRoleId() {
-		return RoleId;
+		return roleId;
 	}
 
 	public void setRoleId(Long roleId) {
-		RoleId = roleId;
+		roleId = roleId;
 	}
-	
+
 }
